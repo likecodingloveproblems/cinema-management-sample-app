9499190594e54de93394532a734daadbf8bd9fdc
likecodingloveproblems <mohammadali.mehdizadeh@outlook.com>
2022-03-28 02:57:23 +0430

    clean up

--- plan/urls.py
+++ plan/urls.py
@@ -1,7 +1,4 @@
-from django.contrib import admin
 from django.urls import path
-from cinema import settings
-from django.conf.urls.static import static
 from plan.views import RoomView, ScheduleView, SeatView
 
 urlpatterns = [
